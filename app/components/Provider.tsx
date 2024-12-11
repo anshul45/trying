b23b4906d6718c92cd0b0782d3bf37b80872ab1e
Anshul45 <anshulgarwal025@gmail.com>
2024-12-11 15:02:09 +0530

    added resizable sidebar to all pages

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -1,15 +1,44 @@
 "use client"
 import { SessionProvider } from "next-auth/react"
 import React from "react"
+import SideBar from "./SideBar"
+import { MantineProvider } from "@mantine/core"
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+import { usePathname } from "next/navigation"
+import '@mantine/core/styles.css';
 
 interface ProvidersProps {
     children:React.ReactNode
 }
 
 const Provider = ({children}:ProvidersProps) => {
+  const path = usePathname();
+
+  const sidebarNotVisible = ["/login","/login/password","/register","/register/password","/"];
+
+  const visible = !sidebarNotVisible.includes(path)
+
   return (
-   <SessionProvider>{children}</SessionProvider>
+    <MantineProvider>
+   <SessionProvider>
+    <PanelGroup
+    direction="horizontal"
+    >
+      {visible && (<>
+        <Panel defaultSizePercentage={19} minSizePercentage={19} maxSizePercentage={30} >
+        <SideBar />
+      </Panel>
+      <PanelResizeHandle  className=" hover:bg-gray-500 w-1"  />
+      </>
+      )} 
+      <Panel defaultSizePercentage={visible ? 81 : 100} minSizePercentage={visible ? 70 : 100} maxSizePercentage={visible ? 81 : 100}>
+    {children}
+      </Panel>
+    </PanelGroup>
+    </SessionProvider>
+    </MantineProvider>
   )
 }
+
 
 export default Provider
---
+++ app/pipeline/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Pipeline = () => {
+
+  return (<>
+   hello
+   </>
+  );
+};
+
+export default Pipeline;
